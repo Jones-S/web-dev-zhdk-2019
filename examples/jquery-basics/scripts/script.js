7a2fe22bd80d219288fb60c70a2372fb101a1721
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-05-15 23:22:53 +0200

    [Update] Examples: Add example how to change css property and toggle a class with jquery

--- examples/jquery-basics/scripts/script.js
+++ examples/jquery-basics/scripts/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
    * http://api.jquery.com/class-selector/
    */
   
-  var myBoxes = $(".box"); // returns object containing all selected elements
+  var myBoxes = $('.box'); // returns object containing all selected elements
   console.log('myBoxes: ', myBoxes);
   
   
@@ -18,17 +18,18 @@ $(document).ready(function() {
    */
   
   
-  $("#fadeButton").click(function(event) {
+  $('#fadeButton').click(function(event) {
     console.log('Fade out that div! 👻');
+    console.log('event.target: ', event.target);
   });
 
   /*
   * Advanced:
   * Exactly the same can be written using arrow functions
-  * Has effect on the context of "this"
+  * Has effect on the context of 'this'
   */
   
-  $("#fadeButton").click((event) => {
+  $('#fadeButton').click((event) => {
     console.log('Ghosting triggered by arrow function 👻');
   });
   
@@ -39,7 +40,7 @@ $(document).ready(function() {
    * Read more: https://api.jquery.com/fadeIn/
    */
   
-  $("#fadeButton").click(function(event) {
+  $('#fadeButton').click(function() {
     $('#divToFade').fadeOut(); // reduces the elements opacity to 0 over time
   });
   
@@ -50,24 +51,30 @@ $(document).ready(function() {
    * Read more: https://api.jquery.com/css/
    */
   
-  $("#changeColorButton").click(function(event) {
-    var currentColor = $('.box').css('background-color');
-    
-    console.log('currentColor: ', currentColor);
-    
-    if (currentColor === 'rgb(0, 140, 255)') {
-      $('.box').css('background-color', 'rgb(72, 236, 167)');
-    } else {
-      $('.box').css('background-color', 'rgb(0, 140, 255)');
-    }
-  }
-    
-    
-    
+   $('#changeColorButton').click(function() {
+     var currentColor = $('#divToDye').css('background-color');     
+     console.log('currentColor: ', currentColor);
+     
+     if (currentColor === 'rgb(0, 140, 255)') {
+       $('#divToDye').css('background-color', 'rgb(72, 236, 167)');
+     } else {
+       $('#divToDye').css('background-color', 'rgb(0, 140, 255)');
+     }
+   });
+   
+  
+  
+  
+  /*
+   * 05: Toggle classes
+   * The neat way to transform layout properties, 
+   * which allow to set transitions easily!
+   * Read more: https://api.jquery.com/toggleClass/
+   */
+  
+  $('#toggleClass').click(function() {
+    var boxesToToggleClass = $('.js-toggle-class-target');
+    boxesToToggleClass.toggleClass('box--highlighted');
   });
-  
-  
-  
-  
   
 });
