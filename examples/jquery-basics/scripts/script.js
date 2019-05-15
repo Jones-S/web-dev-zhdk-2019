15ae2b32c638e2911995cd5c2744012e06bfffd5
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-05-16 00:01:46 +0200

    [Update] Examples: Add more jQuery DOM Manipulation examples

--- examples/jquery-basics/scripts/script.js
+++ examples/jquery-basics/scripts/script.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
   var myBoxes = $('.box'); // returns object containing all selected elements
   console.log('myBoxes: ', myBoxes);
   
+  // We can also log just the first box that is in the collection of boxes found in the DOM
+  console.log('%c myBoxes.first()', 'background: #FDD187; color: #120f9d', myBoxes.first())
+  
   
   
   /*
@@ -75,6 +78,69 @@ $(document).ready(function() {
   $('#toggleClass').click(function() {
     var boxesToToggleClass = $('.js-toggle-class-target');
     boxesToToggleClass.toggleClass('box--highlighted');
+  });
+  
+  
+  
+  /*
+   * 06: Change HTML
+   * Read more: https://api.jquery.com/html
+   */
+  
+  $('#replaceContent').click(function() {
+    var container = $('.js-replace-content');
+    container.html('<p>Brand new content. <em>Granted!</em></p>');
+  });
+  
+  
+  
+  /*
+   * 07: Add HTML to DOM
+   * Read more: https://api.jquery.com/append
+   * Using a multiline HTML string with ECMA-Script 6
+   * template strings.
+   * Read more: https://jack.ofspades.com/multiline-strings-in-es6-javascript/index.html
+   */
+  
+  // Function to create content HTML string
+  createContent = function(title, text) {
+    return `
+    <div class="new-content-wrapper">
+    <h2>${title}</h2>
+    <p>${text}</p>
+    <div class="box-wrapper">
+    <div class="box box--tiny"></div>
+    <div class="box box--tiny"></div>
+    <div class="box box--tiny"></div>
+    </div>
+    </div>`
+  }
+   
+  $('#addContent').click(function() {
+    var container = $('.js-add-content');
+    var titleForContent = 'Title passed to function';
+    var textForContent = 'This can contain more than just one element';
+    
+    // Create new html content by calling the function "createContent"
+    // 👉🏼 Note the parameters (title and text) which we pass to the function
+    var newHTMLContent = createContent(titleForContent, textForContent);
+    
+    console.log('newHTMLContent: ', newHTMLContent);
+    
+    // And add the newly created HTML string as the last part of our container
+    container.append(newHTMLContent);
+  });
+  
+  
+  
+  /*
+   * 07: Remove element
+   * Read more: https://api.jquery.com/category/manipulation/dom-removal/
+   * Using: https://api.jquery.com/remove/
+   */
+  
+  $('#removeDiv').click(function() {
+    $('#divToRemove').remove()
   });
   
 });
