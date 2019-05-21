43b3f6476d59d499e7f0c90507fe45ab085540c7
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-05-21 22:42:57 +0200

    [Update] Pizza-Example: Use a html template to render all ingredients

--- examples/pizza-order/scripts/script.js
+++ examples/pizza-order/scripts/script.js
@@ -2,11 +2,60 @@ $(document).ready(function() {
   
   var ingredients = [
     {
-      name: 'Mozzarella',
-      price: '2.00',
-      currency: 'CHF'
+      name: 'Mozzarella di Buffala',
+      id: 'mozzarella',
+      price: '4.00'
+    },
+    {
+      name: 'Salami',
+      id: 'salami',
+      price: '3.00'
+    },
+    {
+      name: 'Caviar',
+      id: 'caviar',
+      price: '13.00'
+    },
+    {
+      name: 'Truffles',
+      id: 'truffles',
+      price: '18.00'
+    },
+    {
+      name: 'Olives',
+      id: 'olives',
+      price: '1.50'
+    },
+    {
+      name: 'Cherry Tomatoes',
+      id: 'cherry-tomatoes',
+      price: '1.50'
     }
-  ]
+  ];
+  
+  function displayIngredients(ingredients) {
+    var template = $('#template').children();
+    
+    // loop over ingredients
+    for (var i = 0; i < ingredients.length; i++) {
+      // because "template" is always referring to the same instance, we would fill interval
+      // for each ingredient with new content, and we would only see the last one being added
+      var clonedTemplate = template.clone();
+      var populatedTemplate = populateTemplate(clonedTemplate, ingredients[i]);
+      $('#ingredients-wrapper').append(populatedTemplate);
+    }
+    
+    // populate template with data
+    
+  }
+  
+  function populateTemplate(template, ingredient) {
+    template.find('.js-ingredient-name').html(ingredient.name);
+    template.find('.js-ingredient-price').html(ingredient.price);
+    template.find('.js-checkbox').attr('id', ingredient.id);
+    template.find('.js-ingredient-checkbox-label').attr('for', ingredient.id);
+    return template;
+  }
   
   function calculateTotalPrice(ingredients) {
     var totalPrice = 0;
@@ -81,4 +130,6 @@ $(document).ready(function() {
     displayPriceOnScreen(price);
     displaySelectedIngredients(selectedIngredients);
   })
+  
+  displayIngredients(ingredients);
 });
