c28d01668605d43a81e94101352e7ded81aec2b2
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-05-21 23:33:46 +0200

    [Update] Pizza-Example: Refactor to use JS template, use $ to mark jquery objects and build staggered appear animation

--- examples/pizza-order/scripts/script.js
+++ examples/pizza-order/scripts/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   
+  // List of ingredients, that will show on screen after loading the page
   var ingredients = [
     {
       name: 'Mozzarella di Buffala',
@@ -34,36 +35,47 @@ $(document).ready(function() {
   ];
   
   function displayIngredients(ingredients) {
-    var template = $('#template').children();
-    
     // loop over ingredients
     for (var i = 0; i < ingredients.length; i++) {
-      // because "template" is always referring to the same instance, we would fill interval
-      // for each ingredient with new content, and we would only see the last one being added
-      var clonedTemplate = template.clone();
-      var populatedTemplate = populateTemplate(clonedTemplate, ingredients[i]);
-      $('#ingredients-wrapper').append(populatedTemplate);
+      var $populatedTemplate = $(populateTemplate(ingredients[i]));
+      $('#ingredients-wrapper').append($populatedTemplate);
     }
     
-    // populate template with data
+    // fade in all items in a staggered appear animation
+    var $hiddenIngredients = $('#ingredients-wrapper').children();
     
+    console.log('hiddenIngredients: ', $hiddenIngredients)
+    $hiddenIngredients.each(function(index) {
+      // this refers to each single item included in hiddenIngredients
+      var $item = $(this);
+      console.log('this: ', $(this));
+      
+      setTimeout(function() {
+        $item.removeClass('is-hidden');
+      }, index * 100);
+    });
   }
   
-  function populateTemplate(template, ingredient) {
-    template.find('.js-ingredient-name').html(ingredient.name);
-    template.find('.js-ingredient-price').html(ingredient.price);
-    template.find('.js-checkbox').attr('id', ingredient.id);
-    template.find('.js-ingredient-checkbox-label').attr('for', ingredient.id);
+  function populateTemplate(ingredient) {
+    var template = `<li class="js-ingredient ingredient is-hidden">
+      <span class="js-ingredient-name ingredient-name">${ingredient.name}</span>
+      <div>
+        <span class="js-ingredient-price ingredient-price">${ingredient.price}</span> <span class="ingredient-currency">CHF</span>
+      </div>
+      <input class="js-checkbox" type="checkbox" id="${ingredient.id}" name="select" value="selected">
+      <label class="js-ingredient-checkbox-label ingredient-checkbox-label" for="${ingredient.id}">Select</label>
+    </li>`;
     return template;
   }
   
-  function calculateTotalPrice(ingredients) {
+  function calculateTotalPrice($ingredients) {
     var totalPrice = 0;
     
     // jquerys $(selector).each is just like a javascript loop
     // looping over all ingredients
-    ingredients.each(function() {
-      console.log('this: ', this)
+    $ingredients.each(function() {
+      // this refers to each single item included in hiddenIngredients
+      console.log('this: ', this);
       var price = $(this).find('.js-ingredient-price').html();
       
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat
@@ -76,19 +88,19 @@ $(document).ready(function() {
 
   function getSelectedIngredients() {
     // go through each of the ingredients and check if they are selected
-    var allIngredients = $('.js-ingredient');
-    console.log('allIngredients: ', allIngredients);
+    var $allIngredients = $('.js-ingredient');
+    console.log('allIngredients: ', $allIngredients);
     
     // jquerys filter can reduce a collection depending on the return value of a function
     // We now check in this function if the checkbox of this element is selected
     // If it is not, we filter the ingredient out
-    var filteredIngredients = allIngredients.filter(function(index) {
-      console.log('index: ', index)
+    var $filteredIngredients = $allIngredients.filter(function(index) {
+      console.log('index: ', index);
       
-      var checkbox = $(this).find('.js-checkbox');
+      var $checkbox = $(this).find('.js-checkbox');
       
       // reading the checkboxes state
-      var checkboxState = checkbox.is(':checked');
+      var checkboxState = $checkbox.is(':checked');
       console.log('checkboxState: ', checkboxState);
       
       // if we return true, the element is still in the original collection
@@ -96,7 +108,7 @@ $(document).ready(function() {
       return checkboxState;
     });
     
-    return filteredIngredients;
+    return $filteredIngredients;
   }
   
   function displayPriceOnScreen(price) {
@@ -123,12 +135,15 @@ $(document).ready(function() {
   }
   
   $('#calculate-price-button').click(function() {
-    var selectedIngredients = getSelectedIngredients();
-    var price = calculateTotalPrice(selectedIngredients);
+    // prepending a variable name with a $-sign is good practice
+    // to mark with variables contain jQuery objects.
+    // https://stackoverflow.com/questions/205853/why-would-a-javascript-variable-start-with-a-dollar-sign
+    var $selectedIngredients = getSelectedIngredients();
+    var price = calculateTotalPrice($selectedIngredients);
     console.log('price: ', price);
     
     displayPriceOnScreen(price);
-    displaySelectedIngredients(selectedIngredients);
+    displaySelectedIngredients($selectedIngredients);
   })
   
   displayIngredients(ingredients);
