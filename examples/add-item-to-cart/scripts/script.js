eed8303a3fdbf685e81c8cc081b906c1971b3977
Jonas Scheiwiller <git@jonasscheiwiller.ch>
2019-05-30 01:30:15 +0200

    [Update] Add a possible end state of group coding

--- examples/add-item-to-cart/scripts/script.js
+++ examples/add-item-to-cart/scripts/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
   
-  
+  $('.js-button').click(function(event) {
+    var $target = $(event.target);
+    
+    // find the name of the clicked item
+    var $parent = $target.closest('.js-item');
+    console.log('$parent: ', $parent);
+    
+    // get the name of the item
+    var name = $parent.find('.js-item-name').text();
+    console.log('name: ', name)
+    
+    // add item to cart
+    var $cart = $('.js-cart-items');
+    console.log('$cart: ', $cart);
+    
+    var newItem = `<li class="cart-item">${name}</li>`;
+    
+    $cart.append(newItem)
+    
+    // disable button
+    $target.addClass('is-inactive');
+        
+  });
 });
